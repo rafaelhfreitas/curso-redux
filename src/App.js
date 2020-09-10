11cba21a643609b67f54695b991f433d07be0ef7
Rafael Freitas <oraffaudi@gmail.com>
2020-09-09 23:04:21 -0300

    Criação componente card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercicio React - Redux</h1>
+      <div className='Linha'>
+        <Card title="Card 1" red> 
+          X
+        </Card>        
+      </div>
+      <div className='Linha'>
+        <Card title="Card 3" blue>
+          Y
+        </Card>
+      </div>
     </div>
   );
 }
